676c8c8e3e13ae92f943efb17406a57b4052a531
Manuel Venegas <mengasis@outlook.com>
2018-12-08 19:07:25 -0300

    Created queue function

---
+++ src/queue.js
@@ -0,0 +1,21 @@
+function createQueue() {
+  const queue = []
+
+  return {
+    enqueue(item) {
+      queue.unshift(item)
+    },
+    dequeue() {
+      return queue.pop()
+    },
+    peek() {
+      return queue[queue.length - 1]
+    },
+    get length() {
+      return queue.length
+    },
+    isEmpty() {
+      return queue.length == 0
+    }
+  }
+}
