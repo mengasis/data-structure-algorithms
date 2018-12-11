a305ec8c3cf76ef527bc57c419306ae3e38f00dd
Manuel Venegas <mengasis@outlook.com>
2018-12-10 22:01:05 -0300

    Added test to palindrome

--- test/algorithms.test.js
+++ test/algorithms.test.js
@@ -1,7 +1,19 @@
-const { solutionOne } = require('../src/algorithms/reverseString')
+const reverseString = require('../src/algorithms/reverseString')
+const palindrome = require('../src/algorithms/palindrome')
 
 describe('String Reversal', () => {
   test('Should reverse string', () => {
-    expect(solutionOne('Hello World!')).toEqual('!dlroW olleH')
+    expect(reverseString.solutionOne('Hello World!')).toEqual('!dlroW olleH')
+  })
+})
+
+describe('Palindrome', () => {
+  test('Should return true', () => {
+    expect(
+      palindrome.isPalindrome('Cigar? Toss it in a can. It is so tragic')
+    ).toBeTruthy()
+  })
+  test('Should return false', () => {
+    expect(palindrome.isPalindrome('Cut the Gordian knot')).not.toBeTruthy()
   })
 })
