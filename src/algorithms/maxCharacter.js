900cb71559a62e257723d028ee4f5a585dd9ef71
Manuel Venegas <mengasis@outlook.com>
2018-12-12 20:28:49 -0300

    Created maxCharacter function

---
+++ src/algorithms/maxCharacter.js
@@ -0,0 +1,19 @@
+//Return the character that appears the most often.
+
+function maxCharacter(string = '') {
+  let mapModa = {}
+
+  for (const character of string.split('')) {
+    if (character in mapModa) {
+      mapModa[character] += 1
+    } else {
+      mapModa[character] = 1
+    }
+  }
+
+  return Object.entries(mapModa).reduce((a, b) => {
+    return b[1] > a[1] ? b : a
+  })[0]
+}
+
+module.exports = { maxCharacter }
