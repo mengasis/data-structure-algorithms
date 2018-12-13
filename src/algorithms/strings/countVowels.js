58fea8a019d0e0f24e32d51f73eb635e0960598c
Manuel Venegas <mengasis@outlook.com>
2018-12-13 17:24:44 -0300

    Created count vowels function

---
+++ src/algorithms/strings/countVowels.js
@@ -0,0 +1,11 @@
+function countVowels(string = '') {
+  const vowels = ['a', 'e', 'i', 'o', 'u']
+
+  return string.split('').reduce((accumulator, current) => {
+    if (vowels.includes(current)) return (accumulator += 1)
+
+    return accumulator
+  }, 0)
+}
+
+module.exports = { countVowels }
--- src/algorithms/strings/index.js
+++ src/algorithms/strings/index.js
@@ -2,5 +2,6 @@ const { isPalindrome } = require('./palindrome')
 const { getMaxCharacter } = require('./maxCharacter')
 const { reverse } = require('./reverseString')
 const { isAnagram } = require('./anagram')
+const { countVowels } = require('./countVowels')
 
-module.exports = { isPalindrome, getMaxCharacter, reverse, isAnagram }
+module.exports = { isPalindrome, getMaxCharacter, reverse, isAnagram, countVowels }
