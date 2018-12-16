da71eb92d689f644c2e071c2531791cabc37f381
Manuel Venegas <mengasis@outlook.com>
2018-12-16 13:27:12 -0300

    Create stack

---
+++ src/data_structures/stack.js
@@ -0,0 +1,29 @@
+function createStack() {
+  const stack = []
+
+  return {
+    push(x) {
+      stack.push(x)
+    },
+    pop() {
+      if (stack.length === 0) {
+        return undefined
+      }
+      return stack.pop()
+    },
+    peek() {
+      if (stack.length === 0) {
+        return undefined
+      }
+      return stack[stack.length - 1]
+    },
+    get length() {
+      return stack.length
+    },
+    isEmpty() {
+      return stack.length === 0
+    }
+  }
+}
+
+module.exports = {createStack}
