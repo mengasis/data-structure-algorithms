e5ca6e7d372fdcb0ce20218662f87d64138f23ed
Manuel Venegas <mengasis@outlook.com>
2018-12-17 15:10:17 -0300

    Defined first structure of the LinkedList

--- src/data_structures/linkedList.js
+++ src/data_structures/linkedList.js
@@ -1,3 +1,14 @@
 function createNode(value) {
   return { value, next: null }
 }
+
+function createLinkedList() {
+  return {
+    head: null,
+    tail: null,
+    length: 0,
+    isEmpty() {
+      return this.length === 0
+    }
+  }
+}
