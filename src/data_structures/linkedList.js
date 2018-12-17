9a14ebd9f93391a77267b92f5d696e2dfd1dc240
Manuel Venegas <mengasis@outlook.com>
2018-12-17 15:28:10 -0300

    Added condition to "push" method of linked list for when it is null

--- src/data_structures/linkedList.js
+++ src/data_structures/linkedList.js
@@ -13,6 +13,18 @@ function createLinkedList() {
     push(value) {
       const node = createNode(value)
 
+      if (this.head === null) {
+        this.head = node
+        this.tail = node
+        this.length++
+        return node
+      }
+
+      /**
+       * Is set "tail.next" for modify also head, As this they share reference.
+       * Then tail is replaced to a new node, but head still contain the list.
+       */
+      this.tail.next = node
       this.tail = node
       this.length++
     }
