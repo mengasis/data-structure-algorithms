62cc88b6788444a4f2466eef013e7a7b77041950
Manuel Venegas <mengasis@outlook.com>
2018-12-18 02:18:55 -0300

    Added linked method for graph

--- src/data_structures/graph.js
+++ src/data_structures/graph.js
@@ -23,6 +23,17 @@ function createGraph(directed = false) {
     },
     getNode(key) {
       return nodes.find(node => node.key === key)
+    },
+    addEdge(node1Key, node2Key) {
+      const node1 = this.getNode(node1Key)
+      const node2 = this.getNode(node2Key)
+
+      node1.addChildren(node2)
+      edges.push(`${node1Key}${node2Key}`)
+
+      if (!directed) {
+        node2.addChildren(node1)
+      }
     }
   }
 }
